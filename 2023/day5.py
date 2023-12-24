c5235e0e87d9e159eebd7403c2b196ac57fc2bb8
Joaquin Gonzalez <joagonzalez@gmail.com>
2023-12-24 19:39:33 -0300

    progress day5

--- 2023/day5.py
+++ 2023/day5.py
@@ -15,14 +15,57 @@ MAPPERS = [
     'humidity-to-location map',
 ]
 
-def maper_generator(destination: int, source: int, range: int) -> List[int]:
+def maper_generator(
+    input:int, 
+    destination: int, 
+    source: int, 
+    range_map: int) -> List[int]:
     
+    print(f'Input: {input} - Source: {source} - Dest: {destination} - Range: {range_map}')
+    output = None
     
-def maper_parser(data: str): pass
+    source_range = list(range(source, source+range_map))
+    destination_range = list(range(destination, destination+range_map))
+    
+    if input in source_range:
+        tmp = source_range.index(input)
+        output = destination_range[tmp]
+    else:
+        output = input
+        
+    # pprint(source_range)
+    # pprint(destination_range)
+    # print(output)    
+    
+    return output, source_range, destination_range
+
+def maper_parser(data: str):
+    inputs = data[0].split(':')[1].split(' ')
+    inputs = [x for x in inputs if len(x) != 0]
+    print(f'Inputs: {inputs}')
+    outputs = []
+    
+    for i in inputs:
+        for line in data[1:]:
+            if 'map' not in line:
+                params = line.split(' ')
+                params = [int(x) for x in params]
+                
+                if i in list(range(params[1], params[1]+params[2]))
+                    maper_generator(i, params[0], params[1], params[2])
+
+def data_cleaning(data: List[Dict[int, str]]) -> List[Dict[int, str]]:
+    data = [x.replace('\n', '') for x in data]
+    data = [x for x in data if len(x) != 0]
+    
+    return data
 
 def logic():
     data = load_file('data/day5_test')
+    data = data_cleaning(data=data)
     pprint(data)
+    print('----')
+    maper_parser(data=data)
 
 
 if __name__ == '__main__':
